471bc011096fe393446cb52e073c314664934b08
Amir <amaedris1@gmail.com>
2022-09-27 01:26:21 -0700

    The Final Commit Of Pong Gane

--- pongGame/main.py
+++ pongGame/main.py
@@ -2,6 +2,7 @@
 from paddle import Paddle
 from turtle import Screen, Turtle
 from ball import Ball
+from score_board import ScoreBoard
 import time
 
 # Press Shift+F10 to execute it or replace it with your code.
@@ -13,7 +14,7 @@ screen.setup(width=700, height=800)
 screen.title("Pong Game")
 screen.tracer(0)
 
-# creating The  Paddles
+# creating The  Paddles and Positioning it on the screen
 first_paddle = Paddle((340, 0))
 second_paddle = Paddle((-340, 0))
 screen.listen()
@@ -24,7 +25,8 @@ screen.onkey(second_paddle.move_the_paddle_down, "s")
 
 # creating the Ball
 ball = Ball()
-
+# creating Score Board
+score_board = ScoreBoard()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 game_is_on = True
 while game_is_on:
@@ -37,8 +39,17 @@ while game_is_on:
         ball.bounce_y()
 
     # detect collision of the ball to the right paddle
-    if ball.distance(first_paddle) < 50 and ball.xcor() > 340:
+    if ball.distance(first_paddle) < 50 and ball.xcor() > 320 or ball.distance(second_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
+
+    # detect when the first paddle misses the ball
+    if ball.xcor() > 380:
+        ball.reset_position()
+        score_board.left_point()
+    # detect when the second paddle misses the ball
+    if ball.xcor() < -380:
+        ball.reset_position()
+        score_board.right_point()
 
 
 screen.exitonclick()
